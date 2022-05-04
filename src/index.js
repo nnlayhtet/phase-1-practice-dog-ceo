aaa827afdf03ef564b8c0bd9ac093b9e009ccfda
nyinyilayhtet <nnlayhtet@gmail.com>
2022-05-04 17:05:17 -0400

    commit tree df3042495408bfc10da58c9b7b80ce99777a62b2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 console.log('%c HI', 'color: firebrick')
+
+
+function fetchFourDogs(){
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(res=>res.json())
+    .then(data=>downloadImages(data))
+}
+fetchFourDogs()
+
+function fetchDogBreeds(){
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res=>res.json())
+    .then(data=>downloadBreeds(data))
+}
+fetchDogBreeds()
+
+function downloadImages(data){
+    for (let link of data.message){
+    let card = document.getElementById('dog-image-container')
+    let imgTag = document.createElement('img')
+    imgTag.src = link
+    card.append(imgTag)
+    }
+}
+
+function downloadBreeds(data){
+    for (let dogBreed in data.message){
+    let newUi = document.getElementById('dog-breeds')
+    let newLi = document.createElement('li')
+    newLi.textContent = dogBreed
+    newUi.append(newLi)
+    newLi.addEventListener('click',()=>{
+        newLi.style.color = 'red'
+    })
+    }
+}
+
+// let breedSelection = document.getElementById('breed-dropdown')
+// breedSelection.addEventListener('change',(e)=>{
+//     let newValue = e.target.value
+//     if (newValue === 'c'){console.log(downloadBreeds())}
+// }
+// )
